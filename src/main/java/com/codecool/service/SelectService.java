9c30dabe5958db51e1fc2c83266fe1fb3fb262e0
Elzacodecool <elliegolec@gmail.com>
2018-10-26 14:54:37 +0200

    Add select service

---
+++ src/main/java/com/codecool/service/SelectService.java
@@ -0,0 +1,37 @@
+package com.codecool.service;
+
+import com.codecool.converter.Converter;
+import com.codecool.model.Row;
+import com.codecool.model.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+@Service
+public class SelectService implements QueryService {
+    private Converter converter;
+
+    @Autowired
+    public SelectService(Converter converter) {
+        this.converter = converter;
+    }
+
+    @Override
+    public Table executeQuery(String query) {
+        String filename = getFileName(query);
+        Table table = converter.convert("src/main/resources/" + filename);
+        List<String> columnsToDisplay = getListColumns(query);
+        Predicate<Row> predicate = getPredicate(query);
+
+        List<Row> newRows = table.getRows().stream()
+                .filter(predicate)
+                .map(row -> getUpdatedRowWithColumns(row, columnsToDisplay))
+                .collect(Collectors.toList());
+
+        return new Table(columnsToDisplay, newRows);
+    }
+
+}
