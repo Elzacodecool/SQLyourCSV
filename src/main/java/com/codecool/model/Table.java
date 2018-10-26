916209d0b1c0c932ffe909f171f29a70a7275ef3
Elzacodecool <elliegolec@gmail.com>
2018-10-26 13:39:20 +0200

    Add table model

---
+++ src/main/java/com/codecool/model/Table.java
@@ -0,0 +1,32 @@
+package com.codecool.model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Table {
+    private List<String> columnNames;
+    private List<Row> rows;
+
+    public Table(List<String> columnNames, List<Row> rows) {
+        this.columnNames = columnNames;
+        this.rows = rows;
+    }
+
+    public List<String> getColumnNames() {
+        return columnNames;
+    }
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    public String toString() {
+        String columnNamesToString = columnNames.stream()
+                .map(name -> String.format("%"+ Row.COLUMN_WIDTH +"s", name))
+                .collect(Collectors.joining(" | "));
+        String rowsToString = rows.stream()
+                .map(Row::toString)
+                .collect(Collectors.joining("\n"));
+        return columnNamesToString + "\n" + rowsToString;
+    }
+}
