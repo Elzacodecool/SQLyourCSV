4142b3652d02c1a461f150a211ff98a4598223a8
Elzacodecool <elliegolec@gmail.com>
2018-10-26 17:15:34 +0200

    Add test update row with column list

--- src/test/java/com/codecool/service/QueryServiceTest.java
+++ src/test/java/com/codecool/service/QueryServiceTest.java
@@ -1,11 +1,17 @@
 package com.codecool.service;
 
 import com.codecool.exception.WrongQueryFormatException;
+import com.codecool.model.Row;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -21,17 +27,33 @@ class QueryServiceTest {
         String query = "select * from abc.csv";
         assertEquals("abc.csv", queryService.getFileName(query));
     }
-
     @Test
     void testGetFilenameByQuery_withoutFrom() {
         String query = "select * abc.csv";
         assertThrows(WrongQueryFormatException.class, () -> queryService.getFileName(query));
     }
-
     @Test
     void testGetFilenameByQuery_withoutFilename() {
         String query = "select * from";
         assertThrows(WrongQueryFormatException.class, () -> queryService.getFileName(query));
     }
 
+    @Test
+    void testupdateRowWithColumns() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", 1);
+        map.put("first_name", "ala");
+        map.put("age", 20);
+        Row row = new Row(map);
+
+        List<String> columns = Arrays.asList("id", "first_name");
+
+
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("id", 1);
+        map1.put("first_name", "ala");
+        Row expectedRow = new Row(map1);
+
+        assertEquals(expectedRow.toString(), queryService.getUpdatedRowWithColumns(row, columns).toString());
+    }
 }
