941c19cc189e40034444a1b2db75e9b1f4ffb5ba
Elzacodecool <elliegolec@gmail.com>
2018-10-26 13:41:57 +0200

    Add exception when wrong file structure

---
+++ src/main/java/com/codecool/converter/WrongDataStructureException.java
@@ -0,0 +1,7 @@
+package com.codecool.converter;
+
+public class WrongDataStructureException extends RuntimeException {
+    public WrongDataStructureException(String message) {
+        super(message);
+    }
+}
