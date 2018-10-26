a69ba8028103e4e395c84e93484ff2c4b8d4c1cf
Elzacodecool <elliegolec@gmail.com>
2018-10-26 13:40:27 +0200

    Move file reader to converter package

---
+++ src/main/java/com/codecool/converter/FileReader.java
@@ -0,0 +1,24 @@
+package com.codecool.converter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+
+    List<String[]> readData(String file) {
+        List<String[]> data = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.add(line.split(","));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
