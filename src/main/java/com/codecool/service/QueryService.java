5702e3cc7fefb2b19bee4f3be845be3e6ebb0a1c
Elzacodecool <elliegolec@gmail.com>
2018-10-26 15:22:39 +0200

    Add get filename by query

--- src/main/java/com/codecool/service/QueryService.java
+++ src/main/java/com/codecool/service/QueryService.java
@@ -1,7 +1,33 @@
 package com.codecool.service;
 
+import com.codecool.exception.WrongQueryFormatException;
+import com.codecool.model.Row;
 import com.codecool.model.Table;
 
-public interface QueryService {
-    public Table executeQuery(String query);
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public abstract class QueryService {
+    public abstract Table executeQuery(String query);
+
+    protected String getFileName(String query) {
+        List<String> words = Arrays.asList(query.split(" "));
+        int indexFrom = words.indexOf("from");
+        if (indexFrom < 0) {
+            throw new WrongQueryFormatException("Missing FROM statement");
+        } else if (indexFrom >= words.size()) {
+            throw new WrongQueryFormatException("Missing filename");
+        }
+
+        String filename = words.get(indexFrom + 1);
+        if (filename.isEmpty()) {
+            throw new WrongQueryFormatException("Wrong filename");
+        }
+
+        return filename;
+    }
+
+
+
 }
