1a8383bbabb3dc8eb907ab515ac1058e4548c840
Elzacodecool <elliegolec@gmail.com>
2018-10-26 17:00:22 +0200

    Add spring configuration to service

---
+++ src/main/java/com/codecool/service/ServiceConfig.java
@@ -0,0 +1,25 @@
+package com.codecool.service;
+
+import com.codecool.converter.Converter;
+import com.codecool.converter.FileReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceConfig {
+
+    @Bean
+    public QueryService selectService() {
+        return new SelectService(converter());
+    }
+
+    @Bean
+    public Converter converter() {
+        return new Converter(fileReader());
+    }
+
+    @Bean
+    public FileReader fileReader() {
+        return new FileReader();
+    }
+}
