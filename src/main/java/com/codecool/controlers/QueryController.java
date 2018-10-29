914b6c5679c6561b014b76510153663198c8fc1b
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-10-29 14:18:29 +0100

    Basic implementation for form.

--- src/main/java/com/codecool/controlers/QueryController.java
+++ src/main/java/com/codecool/controlers/QueryController.java
@@ -1,9 +1,24 @@
 package com.codecool.controlers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.ui.Model;
 
-@RestController
+@Controller
 @RequestMapping("/query")
 public class QueryController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String displayQuery(Model model) {
+       return "getQuery";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String displayResult(Model model) {
+        return "response";
+    }
+
 }
