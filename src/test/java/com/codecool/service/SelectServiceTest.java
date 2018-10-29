e9a7acbfc9b2977d7be45e6b96fb7902a4a4d39d
Kubset <kuba1228334@gmail.com>
2018-10-29 12:25:16 +0100

    add test execute query with few conditions

--- src/test/java/com/codecool/service/SelectServiceTest.java
+++ src/test/java/com/codecool/service/SelectServiceTest.java
@@ -207,4 +207,24 @@ public class SelectServiceTest {
 
         assertEquals(expectedTable.toString(), service.executeQuery(query).toString());
     }
+
+    @Test
+    void testExecuteQuery_withFewConditions() {
+        String query = "select * from table.csv where id = 1 or first_name like 'marian' and age > 20";
+
+
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("id", 3);
+        map1.put("first_name", "marian");
+        map1.put("age", 90);
+        Row row1 = new Row(map1);
+
+
+        List<String> columnNames = Arrays.asList("id", "first_name", "age");
+        List<Row> rows = Collections.singletonList(row1);
+        Table expectedTable = new Table(columnNames, rows);
+
+        assertEquals(expectedTable.toString(), service.executeQuery(query).toString());
+    }
+
 }
