0d2a75d68e85933223dc03b0dcb0ec04d181bb4a
Kubset <kuba1228334@gmail.com>
2018-10-29 15:46:43 +0100

    add string values to enum, add constructor and getter

--- src/main/java/com/codecool/model/query/SQLAggregateFunctions.java
+++ src/main/java/com/codecool/model/query/SQLAggregateFunctions.java
@@ -1,8 +1,18 @@
-package com.codecool.model;
+package com.codecool.model.query;
 
 public enum SQLAggregateFunctions {
-    SUM,
-    AVG,
-    MIN,
-    MAX;
+    SUM("sum"),
+    AVG("avg"),
+    MIN("min"),
+    MAX("max");
+
+    private String name;
+
+    SQLAggregateFunctions(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
