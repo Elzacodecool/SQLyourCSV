841d1ed92bbd75adfe18cdf0f1f1753aafa2e582
Kubset <kuba1228334@gmail.com>
2018-10-30 14:20:06 +0100

    add cleaned selectService

--- src/main/java/com/codecool/service/SelectService.java
+++ src/main/java/com/codecool/service/SelectService.java
@@ -4,16 +4,19 @@ import com.codecool.converter.Converter;
 import com.codecool.exception.WrongQueryFormatException;
 import com.codecool.model.Row;
 import com.codecool.model.Table;
+import com.codecool.model.query.SelectQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
-public class SelectService extends QueryService {
+public class SelectService {
     private Converter converter;
 
     @Autowired
@@ -21,38 +24,34 @@ public class SelectService extends QueryService {
         this.converter = converter;
     }
 
-    @Override
+
+    public SelectService() {
+    }
+
     public Table executeQuery(String query) {
-        String customizedQuery = query.toLowerCase().replace(";", "");
-        String filename = getFilename(customizedQuery);
-        Table table = converter.convert("src/main/resources/" + filename);
-        List<String> columnsToDisplay = getValidatedListColumns(customizedQuery, table);
-        Predicate<Row> predicate = getPredicate(customizedQuery);
 
-        List<Row> newRows = table.getRows().stream()
-                .filter(predicate)
-                .map(row -> getUpdatedRowWithColumns(row, columnsToDisplay))
-                .collect(Collectors.toList());
+        SelectQuery selectQuery = new SelectQuery(query);
 
-        return new Table(columnsToDisplay, newRows);
+
+
+
+        return null;
+
     }
 
-    String getFilename(String query) {
-        List<String> words = Arrays.asList(query.split(" "));
-        int indexFrom = words.indexOf("from");
-        if (indexFrom < 0) {
-            throw new WrongQueryFormatException("Missing FROM statement");
-        } else if (indexFrom >= words.size() - 1) {
-            throw new WrongQueryFormatException("Missing filename");
-        }
+//    private Table joinTables(Table table1, Table table2, List<String> condition) {
+//
+//        List<String> columns = Stream.concat(table1.getColumnNames().stream(), table2.getColumnNames().stream())
+//                                     .collect(Collectors.toList());
+//
+//        table1.getRows().stream().
+//    }
 
-        String filename = words.get(indexFrom + 1);
-        if (filename.isEmpty()) {
-            throw new WrongQueryFormatException("Wrong filename");
-        }
+//    Row getIncreasedRowWithColumns(Row row1, Row row2, List<String> columns) {
+//        Map<String, Object> rotData = row1.getData().merge(row2.getData());
+//        return new Row(columns.stream().collect(Collectors.toMap(column -> column, column->)))
+//    }
 
-        return filename;
-    }
 
     Row getUpdatedRowWithColumns(Row row, List<String> columns) {
         return new Row(
@@ -61,88 +60,25 @@ public class SelectService extends QueryService {
         );
     }
 
-    List<String> getValidatedListColumns(String query, Table table) {
-        List<String> columns = getListColumns(query);
+//    List<String> getValidatedListColumns(SelectQuery selectQuery, Table table) {
+//        List<String> columns = selectQuery.getAllColumns();
+//
 
-        if (columns.contains("*")) {
-            return table.getColumnNames();
-        }
-
-        if (checkIfColumnsExistInTable(columns, table)) {
-            return columns;
-        }
-
-        throw new WrongQueryFormatException("No column in table");
-    }
-
-    private List<String> getListColumns(String query) {
-        return Arrays.stream(query.split(" "))
-                .filter(word -> query.indexOf(word) > query.indexOf("select"))
-                .filter(word -> query.indexOf(word) < query.indexOf("from"))
-                .map(word -> word.replace(",", ""))
-                .collect(Collectors.toList());
-    }
+//        if (columns.contains("*")) {
+//            return table.getColumnNames();
+//        }
+//
+//        if (checkIfColumnsExistInTable(columns, table)) {
+//            return columns;
+//        }
+//
+//        throw new WrongQueryFormatException("No column in table");
+//    }
 
     private boolean checkIfColumnsExistInTable(List<String> columns, Table table) {
         return table.getColumnNames().containsAll(columns);
     }
 
 
-    Predicate<Row> getPredicate(String query) {
-        if (query.contains("where")) {
-            List<String> queryList = mapQueryToList(query);
-            List<String> condition = queryList.stream()
-                    .skip(queryList.indexOf("where") + 1)
-                    .collect(Collectors.toList());
-            return buildPredicate(condition);
-        }
 
-        return (row) -> true;
-    }
-
-    private Predicate<Row> buildPredicate(List<String> condition) {
-        Predicate<Row> predicate;
-        String columnName = condition.get(condition.size()-3);
-        String operator = condition.get(condition.size()-2);
-        String value = condition.get(condition.size()-1);
-
-        switch (operator) {
-            case "=":
-                predicate = (row) -> row.getData().get(columnName).equals(value);
-                break;
-            case ">":
-                predicate = (row) -> Integer.valueOf(row.getData().get(columnName).toString()) > Integer.valueOf(value);
-                break;
-            case "<":
-                predicate = (row) -> Integer.valueOf(row.getData().get(columnName).toString()) < Integer.valueOf(value);
-                break;
-            case "<>":
-                predicate = (row) -> !row.getData().get(columnName).equals(value);
-                break;
-            case "like":
-                predicate = (row) -> row.getData().get(columnName) instanceof String &&
-                        row.getData().get(columnName).equals(value.replace("\'", ""));
-                break;
-            default:
-                return (row) -> false;
-        }
-
-        if (condition.size() > 3 && condition.get(condition.size()-4).equals("or")) {
-            return predicate.or(buildPredicate(condition.subList(0, condition.size()-4)));
-        } else if (condition.size() > 3 && condition.get(condition.size()-4).equals("and")) {
-            return predicate.and(buildPredicate(condition.subList(0, condition.size()-4)));
-        }
-        return predicate;
-    }
-
-    private List<String> mapQueryToList(String text) {
-        return Arrays.stream(Arrays.stream(text.split(" "))
-                .map(word -> word.length() > 1 ? word.replace("=", " = ") : word)
-                .map(word -> !word.contains("<>") && word.length() > 1 ? word.replace(">", " > ") : word)
-                .map(word -> !word.contains("<>") && word.length() > 1 ? word.replace("<", " < ") : word)
-                .map(word -> word.length() > 2 ? word.replace("<>", " <> ") : word)
-                .collect(Collectors.joining(" "))
-                .split(" "))
-                .collect(Collectors.toList());
-    }
 }
