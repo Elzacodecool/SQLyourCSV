a6186045d9e8b6d21b0265a9688dbd56b71c2a01
Elzacodecool <elliegolec@gmail.com>
2018-10-30 10:59:13 +0100

    Refactor tests names

--- src/test/java/com/codecool/model/query/SelectQueryTest.java
+++ src/test/java/com/codecool/model/query/SelectQueryTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
@@ -16,19 +20,19 @@ class SelectQueryTest {
     private SelectQuery selectQuery;
 
     @Test
-    public void testGetFileName() {
+    public void testFileName() {
         String query = "select * from abc.csv";
         selectQuery = new SelectQuery(query);
 
         assertEquals("abc.csv", selectQuery.getFileNames().get(0));
     }
     @Test
-    public void testGetFilenameByQuery_withoutFrom() {
+    public void testFilenameByQuery_withoutFromStatament() {
         String query = "select * abc.csv";
         assertThrows(WrongQueryFormatException.class, () -> new SelectQuery(query));
     }
     @Test
-    public void testGetFilenameByQuery_withoutFilename() {
+    public void testFilename_withoutFileName() {
         String query = "select * from";
         assertThrows(WrongQueryFormatException.class, () -> new SelectQuery(query));
     }
