5c6c740bebf436e1ec3a524fab6b442fe97e9be3
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-11-10 02:37:32 +0100

    Remove additional lines.

--- src/main/java/com/codecool/model/query/SelectQuery.java
+++ src/main/java/com/codecool/model/query/SelectQuery.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
 
+
 public class SelectQuery {
     private SelectQueryInterpreter interpreter = new SelectQueryInterpreter();
     private SelectQueryValidator validator = new SelectQueryValidator();
