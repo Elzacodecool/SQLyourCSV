981460ba1925383e0704c0ec18afce7fa822d3a7
Elzacodecool <elliegolec@gmail.com>
2018-11-12 14:57:25 +0100

    Add tests for delete query

--- src/main/java/com/codecool/service/DeleteService.java
+++ src/main/java/com/codecool/service/DeleteService.java
@@ -1,13 +1,47 @@
 package com.codecool.service;
 
+import com.codecool.converter.Converter;
+import com.codecool.model.Row;
 import com.codecool.model.Table;
+import com.codecool.model.query.DeleteQuery;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DeleteService extends QueryService {
+    private Converter converter;
+    private DeleteQuery deleteQuery;
+
+    @Autowired
+    public DeleteService(Converter converter, DeleteQuery deleteQuery) {
+        this.converter = converter;
+        this.deleteQuery = deleteQuery;
+    }
+
+    public DeleteService() {
+    }
+
     @Override
     public Table executeQuery(String query) {
-        //TODO
-        return null;
+        deleteQuery.setQuery(query);
+        Table table = converter.convert(deleteQuery.getFileName());
+        Table updatedTable = delete(table);
+        return updatedTable;
+    }
+
+    private Table delete(Table table) {
+        return new Table(table.getColumnNames(), delete(table.getRows()));
+    }
+
+    private List<Row> delete(List<Row> rows) {
+        List<Row> rowsToDelete = rows.stream()
+                .filter(deleteQuery.getWhereCondition())
+                .collect(Collectors.toList());
+        return rows.stream()
+                .filter(row -> !rowsToDelete.contains(row))
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/com/codecool/interpreter/DeleteQueryInterpreterTest.java
+++ src/test/java/com/codecool/interpreter/DeleteQueryInterpreterTest.java
@@ -7,38 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class UpdateQueryInterpreterTest {
+public class DeleteQueryInterpreterTest {
     @Autowired
-    private UpdateQueryInterpreter interpreter;
+    private DeleteQueryInterpreter interpreter;
 
     @Test
     public void testFileName() {
-        String query = "update table set column=column_name, column2 = another_column_name";
+        String query = "delete from table";
 
         assertEquals("table", interpreter.getFilename(query));
     }
 
     @Test
-    public void testSetCondition() {
-        String query = "update table set column=column_name, column2 = another_column_name";
-
-        List<List<String>> expectedConditions = Arrays.asList(
-                Arrays.asList("column", "column_name"),
-                Arrays.asList("column2", "another_column_name")
-        );
-        assertEquals(expectedConditions, interpreter.getSetCondition(query));
-    }
-
-    @Test
     public void testWhereConditionPredicate() {
-        String query = "update table set column=column_name where id = 1 or first_name like 'marian' and age > 20";
+        String query = "delete from table where id = 1 or first_name like 'marian' and age > 20";
 
         Map<String, Object> map1 = new HashMap<>();
         map1.put("id", 3);
